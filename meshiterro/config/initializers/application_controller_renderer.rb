71314595f96bea6b0c786edc3249ff3850725aad
yoshikawakoki <cooler970708@gmail.com>
2021-10-18 06:59:52 +0000

    画像投稿機能をつけよう

--- meshiterro/config/initializers/application_controller_renderer.rb
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '431cd34d21f11b81868854d995bb7fb371ea0a82c5419a75f90922943ba65d5a8012a8bcf0edaa567c23dc884af6f1ce980837ec42c144d63063c47df74b0dbf'
