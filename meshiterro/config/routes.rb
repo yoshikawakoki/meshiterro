c58f26708d2ed6e25b3d9ee1bb2596403aea882a
yoshikawakoki <cooler970708@gmail.com>
2021-10-20 03:49:54 +0000

    プロフィール編集機能

--- meshiterro/config/routes.rb
+++ meshiterro/config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
     resources :post_comments, only: [:create, :destroy]
   end
 
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
